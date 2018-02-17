1f5cf17e9fb77b5058179f738752132bebc48c0c
wk-j <somnuk.wk@gmail.com>
2018-02-18 03:22:58 +0700

    :art: Update styles

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,10 +1,72 @@
+import { EventEmitter } from "events";
 import * as React from "react"
 import * as ReactDOM from "react-dom"
+// tslint:disable-next-line:no-unused-expression
+import "./Style.css"
+// tslint:disable-next-line:max-line-length
+const line = "🎨⚡🔥🐛🚑✨📝🚀💄🎉✅🔒🍎🐧🏁🤖🍏🔖🚨🚧💚⬇⬆📌👷📈♻➖🐳➕🔧🌐✏💩⏪🔀📦👽🚚📄💥🍱👌♿💡🍻💬🗃🔊🔇👥🚸🏗📱🤡"
+const mojis = Array.from(line)
 
-class App extends React.Component<{}, {}> {
+mojis.forEach(x => console.log(x))
+
+type State = {
+    hover: ""
+}
+
+class App extends React.Component<{}, State> {
+
+    constructor(props) {
+        super(props)
+        this.state = { hover: "" }
+    }
+
+    private onMouseEnter = (text) => (e) => {
+        this.setState({ hover: text })
+    }
+
+    private onMouseLeave = (e) => {
+    }
+
+    private Moji = ({ item }: { item: string }) =>
+        <span style={{
+            width: "30px",
+            height: "30px",
+            fontSize: "25px",
+            cursor: "pointer",
+            margin: "2px",
+            padding: "3px",
+            textShadow: this.state.hover === item ? "4px 4px 4px #000000" : "2px 2px 4px #000000"
+        }} onMouseEnter={this.onMouseEnter(item)} key={item}>
+            {item}
+        </span>
+
+    private MojiContainer = ({ items }: { items: string[] }) =>
+        <div style={{
+            margin: "10px"
+        }}>
+            {items.map(x => <this.Moji item={x} />)}
+        </div>
+
+    private InputContainer = () =>
+        <div style={{ width: "100%", background: "green" }}>
+            <textarea style={{
+                padding: "8px",
+                fontSize: "25px",
+                width: "100%",
+                fontFamily: "NewRocker",
+                // color: "Fuchsia",
+                border: "1px solid grey",
+                boxSizing: "border-box",
+                textShadow: "2px 2px 4px #000000"
+            }} />
+        </div>
+
     public render() {
         return (
-            <h1>Hello</h1>
+            <div style={{ height: "100%" }}>
+                <this.MojiContainer items={mojis} />
+                <this.InputContainer />
+            </div>
         )
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
         }])
     ],
     resolve: {
-        extensions: [".ts", ".tsx", ".js"]
+        extensions: [".ts", ".tsx", ".js", ".css"]
     },
     externals: {}
 };
